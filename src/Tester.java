234b45e74fd924c4c54f808858f4a61968b36d45
OmobolajiA <hellibolaji@gmail.com>
2025-06-28 21:45:37 +0200

    Initial commit

---
+++ src/Tester.java
@@ -0,0 +1,8 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] number = {10, 20, 30, 40, 50};
+        for (int x : number) {
+            System.out.println(x);
+        }
+    }
+}
